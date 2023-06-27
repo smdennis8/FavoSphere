1fcabbe6d4188eb23b7807f3687fa6842c0c2145
Zack <zda1994@gmail.com>
2023-06-27 12:32:52 -0400

    Add Security Package

--- favosphere-server/src/main/java/favorite/security/SecurityConfig.java
+++ favosphere-server/src/main/java/favorite/security/SecurityConfig.java
@@ -1,4 +1,52 @@
 package favorite.security;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@ConditionalOnWebApplication
 public class SecurityConfig {
+
+    private final JwtConverter jwtConverter;
+
+    public SecurityConfig(JwtConverter jwtConverter) {
+        this.jwtConverter = jwtConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authConfig) throws Exception {
+
+        http.csrf().disable();
+
+        http.cors();
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,
+                        "/security/authenticate", "/security/create-account").permitAll()
+                .antMatchers(HttpMethod.POST, "/security/refresh-token").authenticated()
+                .antMatchers(HttpMethod.GET,
+                        "/favorite", "/favorite/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/favorite").authenticated()
+                .antMatchers(HttpMethod.PUT, "/favorite/*").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/favorite/*").hasAuthority("ADMIN")
+                .antMatchers("/**").denyAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(authConfig), jwtConverter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
