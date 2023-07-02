d8e6d859e47b662565f5c578d7f0fa0e67b95be8
Zack <zda1994@gmail.com>
2023-07-02 00:27:28 -0400

    Edit 'Edit' Button

--- favosphere-client/src/components/LeftPanel.js
+++ favosphere-client/src/components/LeftPanel.js
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 
-const LeftPanel = (props) => {
+const LeftPanel = () => {
     return (
         <div className="leftPanel">
-                <Link to={'/'} a href="home">Home</Link>
-                <Link to={'/'} a href="profile">Profile</Link>
-                <Link to={'/gallery'} a href="gallery">Favorites Gallery</Link>
-                <Link to={'/gallery'} a href="table">Favorites Table</Link>
-                <Link to={'/'} a href="share">Share Favorites</Link>
-                <Link to={'/gallery'} a href="staging">Favorites Staging</Link>
-                <Link to={'/'} a href="contact">Contact</Link>
+                <Link to={'/'} href="home">Home</Link>
+                <Link to={'/'} href="profile">Profile</Link>
+                <Link to={'/gallery'} href="gallery">Favorites Gallery</Link>
+                <Link to={'/gallery'} href="table">Favorites Table</Link>
+                <Link to={'/'} href="share">Share Favorites</Link>
+                <Link to={'/gallery'} href="staging">Favorites Staging</Link>
+                <Link to={'/'} href="contact">Contact</Link>
         </div>
     );
 };
