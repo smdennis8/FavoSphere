b4dff24115b2a9bd2c560409c3b8ff1e100835f2
sirakn2 <80297789+sirakn2@users.noreply.github.com>
2023-07-03 11:14:25 -0500

    Bug fix for adding favorite

--- favosphere-client/src/components/Errors.js
+++ favosphere-client/src/components/Errors.js
@@ -1,11 +1,20 @@
-function Errors(){
-    return(<>
-    <main className="errors">
-        <div>
-            <p></p>
-        </div>
-    </main>
-    </>)
+function Errors({ errors }) {
+
+    const getErrorList = (err) => {
+        if (Array.isArray(err)) {
+            return err;
+        } else {
+            return [err];
+        }
+    }
+
+    return <>
+        {getErrorList(errors).length > 0 && <div className="alert alert-danger">
+            <ul>
+                {getErrorList(errors).map(err => <li key={err}>{err}</li>)}
+            </ul>
+        </div>}
+    </>
 }
 
 export default Errors;
