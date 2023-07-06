5f040228910853ee58281d0a28d92376b15b5fa0
sirakn2 <80297789+sirakn2@users.noreply.github.com>
2023-07-06 03:33:07 -0500

    Some more styling added

--- favosphere-client/src/components/FavoriteGallery.js
+++ favosphere-client/src/components/FavoriteGallery.js
@@ -49,44 +49,45 @@ function FavoriteGallery() {
     }, [favorites.length]);
 
     return(<>
-        <div className="row">
-            <div className="col">
-            <Link to="/add" className="btn btn-dark">Add New Favorite</Link>
-            </div>
-        </div>
-
-        {favorites.length > 0 ? 
-        <Grid container spacing={4} columnSpacing={{ xs: 5 }}>
-            {favorites.map(f => (
-                <Grid item md={4}>
-                    <div className="card col-sm-6 col-lg-4" key={f.favoriteId}>
-                    {auth.isLoggedIn() &&
-                    <Link to={`/card/${f.favoriteId}`}>
-                        {f.imageUrl !== null && f.imageUrl !== "" &&
-                        <img src={f.imageUrl} className="card-img-top" alt={"Title image for " + f.title} />}
-                        {f.gifUrl !== null &&  f.gifUrl !== "" &&
-                        <img src={f.gifUrl} className="card-gif-mid" alt={"Title gif for " + f.title} />}
-                        <div className="card-body">
-                            <h1 className="card-title">{f.title}</h1>
-                            <h2 className="card-source">Source:<p>{f.source}</p></h2>
-                            <h2 className="card-creator">By:<p>{f.creator}</p></h2>
-                            <h2 className="card-type">Type:<p>{f.type}</p></h2>
-                            <h2 ref={descriptionRef} className="card-description">Description:<p>{f.description}</p></h2>
-                            <h3 className="card-createdOn">Created On: {f.createdOn}</h3>
-                            <h3 className="card-updatedOn">Updated On: {f.updatedOn}</h3>
-                        </div>
-                    </Link>}
+    {favorites.length > 0 &&
+    <div className="gallery-title">
+        <h1>NOW ALL YOUR FAVS IN ONE PLACE...</h1>
+    </div>}
+    {favorites.length > 0 ? 
+    <Grid container spacing={4} columnSpacing={{ xs: 5 }}>
+        {favorites.map(f => (
+            <Grid item md={4}>
+                <div className="card col-sm-6 col-lg-4" key={f.favoriteId}>
+                {auth.isLoggedIn() &&
+                <Link to={`/card/${f.favoriteId}`}>
+                    {f.imageUrl !== null && f.imageUrl !== "" &&
+                    <img src={f.imageUrl} className="card-img-top" alt={"Image for favorite titled " + f.title} />}
+                    {f.gifUrl !== null &&  f.gifUrl !== "" &&
+                    <img src={f.gifUrl} className="card-gif-mid" alt={"Gif for favorite titled " + f.title} />}
+                    <div className="card-body">
+                        <h1 className="card-title">{f.title}</h1>
+                        <h2 className="card-source">Source: {f.source}</h2>
+                        <h2 className="card-creator">By: {f.creator}</h2>
+                        <h2 className="card-type">Favorite Item Type: {f.type}</h2>
+                        <p></p>
+                        <h2 ref={descriptionRef} className="card-description">Description:<p></p><p>{f.description}</p></h2>
                     </div>
-                </Grid>))}
-        </Grid>
-        : 
-        <div className="col-sm-6 col-lg-4">No Favorites</div>}
-        <div className="row">
-            {location.state && 
-            <div className="alert alert-info">
-                {location.state.msg}
-            </div>}
-        </div>
+                    <div className="card-footer">
+                        <h3 className="card-createdOn">Created On: {f.createdOn}</h3>
+                        <h3 className="card-updatedOn">Updated On: {f.updatedOn}</h3>
+                    </div>
+                </Link>}
+                </div>
+            </Grid>))}
+    </Grid>
+    : 
+    <div className="col-sm-6 col-lg-4">No Favorites</div>}
+    <div className="row">
+        {location.state && 
+        <div className="alert alert-info">
+            {location.state.msg}
+        </div>}
+    </div>
     </>);
 }
 
